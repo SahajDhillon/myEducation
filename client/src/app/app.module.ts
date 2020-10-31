c40f1a3a085f33e1525cae2e268048b9c9ff83de
SahajDhillon <sdsdsahaj@gmail.com>
2020-10-31 22:01:43 +0530

    myEducation(Part 9)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,8 +10,8 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { MemberListComponent } from './member-list/member-list.component';
-import { MemberDetailComponent } from './member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MyskillsComponent } from './myskills/myskills.component';
@@ -24,6 +24,8 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -41,7 +43,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     MyresultsComponent,
     TestErrorsComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
@@ -52,7 +55,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     SharedModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS,useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS,useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS,useClass: JwtInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
